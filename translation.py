0b22a350feac5d78ee0d05252d704cbf7b0d7b39
Rzky3016 <83935714+Rzky3016@users.noreply.github.com>
2021-08-05 10:44:46 +0700

    UPDATE

--- translation.py
+++ translation.py
@@ -10,7 +10,7 @@ Kode ini hanya digunakan untuk tujuan mendapatkan ID APP dari my.telegram.org
 jika Anda tidak mempercayai dev bot ini, Ambil Manual Aja Tlol, Dikasih Yang Gampang Milihnya Yang Susah 🙄 😁🙏
 """
     BEFORE_SUCC_LOGIN = "Kode Diterima. Scarpping Web Page..."
-    ERRED_PAGE = "Hamdeh Error. Gagal Mendapatkan id aplikasi. \n\n Kontak saya : @mrismanaziz"
+    ERRED_PAGE = "Hamdeh Error. Gagal Mendapatkan id aplikasi. \n\n Kontak saya : @Nopegoodloking"
     CANCELLED_MESG = "Bye! Silahkan /start kembali untuk mengulang"
     IN_VALID_CODE_PVDED = "Masukan Kode Telegram yang Bener Tlol 😁🙏"
     IN_VALID_PHNO_PVDED = "Masukan NO HP yang Bener Tlol 😁🙏 Pake Format +(kode Negara) Contoh +62(nomer hp)"
